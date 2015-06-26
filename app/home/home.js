9a15779f6f7d145350c9f70b0bf53274151457b5
gandhark <gandhark@cybage.com>
2015-06-26 12:57:46 +0530

    working code

--- app/home/home.js
+++ app/home/home.js
@@ -10,14 +10,62 @@ angular.module('myApp.home', ['ngRoute','firebase'])
 }])
 
 .controller('HomeCtrl', ['$scope','$firebaseSimpleLogin',function($scope,$firebaseSimpleLogin) {
-  var firebaseObj = new Firebase("https://blistering-heat-2473.firebaseio.com");
-var loginObj = $firebaseSimpleLogin(firebaseObj);
+  var firebaseObj = new Firebase("https://sizzling-heat-6000.firebaseio.com/");
+
+
+
+
+
+firebaseObj.child("Questions/QProjectTeam/QPT1").on("value", function(snapshot) {
+  alert(snapshot.val());  // Alerts "San Francisco"
+ // $scope.proj.DevNo=snapshot.val();
+   //alert($scope.proj.DevNo);
+});
+   
+   
+   
+ 
+  firebaseObj.child("Questions/QProjectTeam/QPT2").on("value", function(snapshot) {
+  alert(snapshot.val());  // Alerts "San Francisco"
+  //$scope.proj.TestNo=snapshot.val();
+   //alert($scope.proj.TestNo);
+  });
   
+  firebaseObj.child("Questions/QProjectTeam/QPT3").on("value", function(snapshot) {
+  alert(snapshot.val());  // Alerts "San Francisco"
+ // $scope.proj.TL=snapshot.val();
+   //alert($scope.proj.TL);
+  });
+  
+  
+  firebaseObj.child("Questions/QProjectTeam/QPT4").on("value", function(snapshot) {
+  alert(snapshot.val());  // Alerts "San Francisco"
+ // $scope.proj.PM=snapshot.val();
+ //  alert($scope.proj.PM);
+});
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+  var loginObj = $firebaseSimpleLogin(firebaseObj);
   $scope.user = {};
   $scope.SignIn = function(e){ 
      e.preventDefault();
      var username = $scope.user.email;
      var password = $scope.user.password;
+	 
      loginObj.$login('password', {
                 email: username,
                 password: password
